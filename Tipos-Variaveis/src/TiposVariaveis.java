b4adb15f10506b2401f4e1260d56a03997a508ea
antonio <antoniopedro322@gmail.com>
2024-06-16 11:35:51 -0300

    Update TiposVariaveis.java

--- Tipos-Variaveis/src/TiposVariaveis.java
+++ Tipos-Variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        double salarioMinimo = 2500.00;
+
+        //Casting
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        numeroCurto = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+
+        //Para constante use caps lock
+        double VALOR_DE_PI = 3.14;
+
+        String meuNome = "Antonio Pedro";
+        //String é uma classe, e não um tipo primitivo
+
     }
 }
