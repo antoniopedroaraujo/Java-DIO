a372daed1bf5c64904ae46674d7df1b91a7f329d
antonio <antoniopedro322@gmail.com>
2024-06-18 10:52:36 -0300

    Update Controle-de-Fluxo

---
+++ Controle-de-Fluxo/src/ExemploForArray.java
@@ -0,0 +1,12 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos[]={"FELIPE","JONAS","JULIA","MARCOS"};
+
+        for (int i = 0; i < alunos.length; i++) {
+            System.out.println("O aluno no índice x="+i+" é igual a "+alunos[i]);
+        }
+        for(String aluno : alunos){
+            System.out.println("Nome do aluno é: "+aluno);
+        }
+    }
+}
